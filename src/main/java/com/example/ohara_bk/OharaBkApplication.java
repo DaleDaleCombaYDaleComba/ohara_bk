f6960745e9ae7d65c9fb8270a9f9f47873090a3f
Juanfri <jhernandezdominguez@safareyes.es>
2024-04-04 22:59:54 +0200

    Initial commit

---
+++ src/main/java/com/example/ohara_bk/OharaBkApplication.java
@@ -0,0 +1,13 @@
+package com.example.ohara_bk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OharaBkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OharaBkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ohara_bk/OharaBkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ohara_bk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OharaBkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
